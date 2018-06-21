d7eb58a326c28240fa069438e7ff32a916010210
Alen Duda <alenduda@gmail.com>
2018-06-21 13:22:55 +0200

    Workaround for fetching GitHub repositories

--- script.js
+++ script.js
@@ -3,40 +3,49 @@ window.addEventListener("load", getRepositories);
 function getRepositories() {
   let repoList = document.getElementById("repoList");
   let output = "";
-  //if (self.fetch) {
+  // get all repos
   fetch("https://api.github.com/users/aduda091/repos?sort=pushed")
     .then(res => res.json())
     .then(repos => {
       for (let repo of repos) {
+        // prepare a div element to output found data
+        let repoDomItem = document.createElement("div");
+        repoDomItem.className = "repo";
+
+        // begin generating output (Firefox workaround)
+        let itemHtml = `
+          <h4 class="repoName"><a href="${repo.html_url}"> ${repo.name}</a></h4>
+          <div>
+            <p class="repoDescription"> ${
+              repo.description ? repo.description : ""
+            }  </p>
+            `;
+
+        // find URL of the current repo languages
         let langUrl = repo.languages_url;
+        // get current repo languages
         fetch(langUrl)
           .then(langs => langs.json())
           .then(languages => {
+            // create an array of languages
             let langArr = Array.from(Object.keys(languages));
+            // join them with a comma
             let langString = langArr.join(", ");
-            let item = `
-					<div class="repo">	
-							<h4 class="repoName"><a href="${repo.html_url}"> ${repo.name}</a></h4>
-						<div>
-							<p class="repoDescription"> ${repo.description ? repo.description : ""}  </p>
-							<p class="repoLanguages">(${langString})</p>
-						</div>
-				</div>
-			`;
-            output += item;
-          })
-          .catch(error => {
-            alert(error);
-          })
-          .finally(() => {
-            //finally is not supported by some browsers
-            repoList.innerHTML = output;
+            // output languages to the DOM node
+            itemHtml += `
+                <p class="repoLanguages">(${langString})</p>
+              </div>
+            `;
+            // finally, update the DOM with complete output of the repo data
+            repoDomItem.innerHTML = itemHtml;
+            repoList.appendChild(repoDomItem);
           });
       }
     })
     .catch(error => {
-      repoList.innerHTML =
-        "<p>Unable to fetch repositories at this moment, please use the GitHub link above.</p>";
+      // hide visual evidence of any errors
+      repoList.remove();
+      document.getElementById("repoListTitle").remove();
       console.log(error);
     });
 }
